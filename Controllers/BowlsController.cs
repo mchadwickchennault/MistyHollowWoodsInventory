062eb9483909e7ada80a0902cc970a24559d66a9
Morris Chennault <mchadwickchennault@gmail.com>
2022-01-03 19:38:18 -0800

    Update Controller

--- Controllers/BowlsController.cs
+++ Controllers/BowlsController.cs
@@ -54,7 +54,7 @@ namespace MistyHollowWoodsInventory.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Description,WoodSpecies,BowlType,Price,Sold")] Bowls bowls)
+        public async Task<IActionResult> Create([Bind("ID,Description,Location,WoodSpecies,BowlType,Price,Sold")] Bowls bowls)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace MistyHollowWoodsInventory.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Description,WoodSpecies,BowlType,Price,Sold")] Bowls bowls)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Description,Location,WoodSpecies,BowlType,Price,Sold")] Bowls bowls)
         {
             if (id != bowls.ID)
             {
